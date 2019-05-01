c943c84f3761d66605aee325207c0a37f45f965c
Nick810 <cbnickp@gmail.com>
2019-05-01 19:25:06 +0700

    Update application.py

--- application.py
+++ application.py
@@ -1,20 +1,49 @@
 import os
-import constants
-constants.print_team()
+from constants import TEAMS, PLAYERS
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def team_builder():
+def panthers_stats():
+    panthers_team = PLAYERS[:6]
+    print("Team: {} Stats".format(TEAMS[0]))
+    print("-"*24)
+    print("Total players {}\n".format(len(panthers_team)))
+    print("Players on Team:")
+    print("")
+    enterToContinue()
 
-    Panthers = []
-    Bandits = []
-    Warriors = []
+
+def bandits_stats():
+    bandits_team = PLAYERS[6:12:]
+    print("Team: {} Stats".format(TEAMS[1]))
+    print("-"*24)
+    print("Total players {}\n".format(len(bandits_team)))
+    print("Players on Team:")
+    print("")
+    enterToContinue()
+
+
+def warriors_stats():
+    warriors_team = PLAYERS[-6:]
+    print("Team: {} Stats".format(TEAMS[2]))
+    print("-"*24)
+    print("Total players {}\n".format(len(warriors_team)))
+    print("Players on Team:")
+    print("")
+    enterToContinue()
+
+
+def enterToContinue():
+    while True:
+        moveOn = input("Press ENTER to continue...")
+        if moveOn == "":
+            clear_screen()
+            teams_options()
+        elif moveOn == str or int:
+            print("You didn't press ENTER. Please press ENTER to continue")
     
-    with open("constants.py") as file:
-        for index in file:
-            print(index)
 
 def teams_options():
     while True:
@@ -24,11 +53,11 @@ def teams_options():
             print("3) Warriors\n")
             team_choice = int(input("Enter an option > "))
             if team_choice == 1:
-                panthers_stat()  # A Function here
+                panthers_stats()  # A Function here
             elif team_choice == 2:
-                bandit_stat()  # A Function here
+                bandits_stats()  # A Function here
             elif team_choice == 3:
-                warrior_stat()  # A Function here
+                warriors_stats()  # A Function here
             else:
                 print("That's not a valid option. Please enter a number either 1 to 3.")
         except ValueError:
